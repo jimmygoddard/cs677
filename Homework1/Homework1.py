501850cf42c14fa6821c601e77b93513659d075e
Jimmy Goddard <jimmy.goddard@gmail.com>
2019-02-02 17:36:40 -0500

    - Refactored some weekday-based functionality into functions and generalized it so that it could be used for months - Finished Question 2

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -23,59 +23,67 @@ with open(output_file) as in_file:
     # split csv file into a list of lists.  first item in the list are the headers
     lines = [line.split(',') for line in in_file.read().splitlines()]
 
-# construct a dictionary of weekday name to list of return values
-weekday_dict = {}
-weekday_idx = None
-return_idx = None
-for i, line in enumerate(lines):
-    if i == 0:
-        weekday_idx = line.index('Weekday')
-        return_idx = line.index('Return')
-        continue
-    weekday = line[weekday_idx]
-    return_value = float(line[return_idx])
-    if weekday in weekday_dict:
-        weekday_dict[weekday].append(return_value)
-    else:
-        weekday_dict[weekday] = [return_value]
 
-# construct a dictionary of weekday name to dictionary of min, max, average, and median
-weekday_values = {}
-for day in weekday_dict:
-    returns = weekday_dict[day]
-    weekday_values[day] = {
-        'min': min(returns),
-        'max': max(returns),
-        'average': statistics.mean(returns),
-        'median': statistics.median(returns)
-    }
+def construct_returns_dict(by_column, key_formatter=lambda x: x):
+    returns_dict = {}
+    by_column_idx = None
+    return_idx = None
+    for i, line in enumerate(lines):
+        if i == 0:
+            by_column_idx = line.index(by_column)
+            return_idx = line.index('Return')
+            continue
+        key = key_formatter(line[by_column_idx])
+        return_value = float(line[return_idx])
+        if key in returns_dict:
+            returns_dict[key].append(return_value)
+        else:
+            returns_dict[key] = [return_value]
+    return returns_dict
+
+
+def construct_values_dict(returns_dict):
+    values_dict = {}
+    for key in returns_dict:
+        returns = returns_dict[key]
+        values_dict[key] = {
+            'min': min(returns),
+            'max': max(returns),
+            'average': statistics.mean(returns),
+            'median': statistics.median(returns)
+        }
+    return values_dict
+
+
+def get_max_average(values_dict):
+    average_dict = {'key': None, 'max': None}
+    for key in values_dict:
+        if average_dict['max'] is None:
+            average_dict['max'] = values_dict[key]['average']
+        elif values_dict[key]['average'] > average_dict['max']:
+            average_dict['max'] = values_dict[key]['average']
+            average_dict['key'] = key
+    return average_dict
+
+
+weekday_dict = construct_returns_dict('Weekday')
+weekday_values = construct_values_dict(weekday_dict)
 
 print('{:9}\t{:6}\t{:6}\t{:6}\t{:6}'.format('Weekday', 'min', 'max', 'average', 'median'))
 for day in weekday_values:
     print('{weekday:9}\t{min:.2f}\t{max:.2f}\t{average:.2f}\t{median:.2f}'.format(weekday=day, **weekday_values[day]))
 
-max_average = {'weekday': None, 'max': -10e25}
-for day in weekday_values:
-    if weekday_values[day]['average'] > max_average['max']:
-        max_average['max'] = weekday_values[day]['average']
-        max_average['weekday'] = day
 
-print('{weekday} has the highest average of {max}'.format(**max_average))
+max_weekday_avg = get_max_average(weekday_values)
+print('{key} has the highest average of {max}'.format(**max_weekday_avg))
 
+month_dict = construct_returns_dict('Month', lambda idx: calendar.month_name[int(idx)])
+month_values = construct_values_dict(month_dict)
 
-# construct a dictionary of month name to list of return values
-month_dict = {}
-month_idx = None
-return_idx = None
-for i, line in enumerate(lines):
-    if i == 0:
-        month_idx = line.index('Month')
-        return_idx = line.index('Return')
-        continue
-    month = calendar.month_name[int(line[month_idx])]
-    return_value = float(line[return_idx])
-    if month in month_dict:
-        month_dict[month].append(return_value)
-    else:
-        month_dict[month] = [return_value]
+print()
+print('{:9}\t{:6}\t{:6}\t{:6}\t{:6}'.format('Month', 'min', 'max', 'average', 'median'))
+for month in month_values:
+    print('{month:9}\t{min:.2f}\t{max:.2f}\t{average:.2f}\t{median:.2f}'.format(month=month, **month_values[month]))
 
+max_month_avg = get_max_average(month_values)
+print('{key} has the highest average of {max}'.format(**max_month_avg))
