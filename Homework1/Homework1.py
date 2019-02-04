9df572391c5815a53d375982cf5a71ee580bdfc2
Jimmy Goddard <jimmy.goddard@gmail.com>
2019-02-03 21:29:03 -0500

    - Added docstrings to Homework1

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -1,3 +1,8 @@
+"""
+Jimmy Goddard
+CS 677 Homework 1
+2/2/19
+"""
 import calendar
 import itertools
 import operator
@@ -8,6 +13,10 @@ from orig_stock_data import get_stock
 
 
 def get_data_table():
+    """
+    Retrieves stock data, writes it to a CSV file and returns it as a matrix
+    :return: data table matrix
+    """
     # ticker = 'GS'  # Goldman Sachs Group Inc
     # ticker = 'GDDY'  # GoDaddy
     # ticker = 'GM'  # General Motors
@@ -16,7 +25,10 @@ def get_data_table():
     end_date = '2018-12-31'
     s_window = 14
     l_window = 50
-    input_dir = r'C:\Users\jimmy_000\src\git\cs677\datasets'
+    home_dir = os.path.join(os.path.sep + 'home', 'jgoddard')
+    # home_dir = os.path.join('C:', os.path.sep, 'Users', 'jimmy_000')
+    input_dir = os.path.join(home_dir, 'src', 'git', 'CS677', 'datasets')
+    # input_dir = r'C:\Users\jimmy_000\src\git\cs677\datasets'
     output_file = os.path.join(input_dir, ticker + '.csv')
 
     if not os.path.isfile(output_file):
@@ -31,6 +43,14 @@ def get_data_table():
 
 
 def construct_returns_dict(data_table, by_column, key_formatter=lambda x: x):
+    """
+    Construct a dictionary from stock data matrix grouped by the by_column with a list of returns as the values
+    :param data_table: matrix of stock data with the first row being the headers
+    :param by_column: column to group by
+    :param key_formatter: optional argument which will be used to format the by_column before it becomes a key in the
+    returns dictionary
+    :return: a dictionary containing the groupings from by_column of returns values
+    """
     returns_dict = {}
     by_column_idx = None
     return_idx = None
@@ -49,6 +69,11 @@ def construct_returns_dict(data_table, by_column, key_formatter=lambda x: x):
 
 
 def construct_values_dict(returns_dict):
+    """
+    Construct a dictionary of returns statistics for each grouping created by construct_returns_dict
+    :param returns_dict: the output of the construct_returns_dict function
+    :return: a dictionary containing statistics for each returns list in the returns_dict
+    """
     values_dict = {}
     for key in returns_dict:
         returns = returns_dict[key]
@@ -62,6 +87,12 @@ def construct_values_dict(returns_dict):
 
 
 def get_max_average(values_dict):
+    """
+    Contruct a dictionary containing the max value and which grouping it was for
+    :param values_dict: output of construct_values_dict function
+    :return: a dictionary with a "max" key and a "key" key which identifies which grouping in values_dict had the max
+    average
+    """
     average_dict = {'key': None, 'max': None}
     for key in values_dict:
         if average_dict['max'] is None:
@@ -73,6 +104,11 @@ def get_max_average(values_dict):
 
 
 def print_weekday_analysis(weekday_values):
+    """
+    Helper function to print out the weekdays returned by the construct_values_dict function
+    :param weekday_values: dictionary returned by the construct_values_dict when using by_column of 'Weekday'
+    :return: void
+    """
     print()
     print('{:9}\t{:6}\t{:6}\t{:6}\t{:6}'.format('Weekday', 'min', 'max', 'average', 'median'))
     for day in weekday_values:
@@ -84,6 +120,11 @@ def print_weekday_analysis(weekday_values):
 
 
 def print_month_analysis(month_values):
+    """
+    Helper function to print out the months returned by the construct_values_dict function
+    :param month_values: dictionary returned by the construct_values_dict when using by_column of 'Month'
+    :return: void
+    """
     print()
     print('{:9}\t{:6}\t{:6}\t{:6}\t{:6}'.format('Month', 'min', 'max', 'average', 'median'))
     for month in month_values:
@@ -95,6 +136,11 @@ def print_month_analysis(month_values):
 
 
 def get_runs(data_table):
+    """
+    Return a list of lists containing the indexes of all negative Return runs in the data table
+    :param data_table: a matrix of stock values with the first row being the headers
+    :return: a list of lists containing the row indexes for the data table of all negative Return runs in the data table
+    """
     return_idx = data_table[0].index('Return')
     neg_indexes = [i + 1 for i, item in enumerate(data_table[1:]) if float(item[return_idx]) < 0]
 
@@ -108,12 +154,26 @@ def get_runs(data_table):
 
 
 def get_run_end_indexes(runs, w):
+    """
+    Given a list of lists of runs and a window size, return all indexes that mark the of a run of that window size
+    :param runs: list of lists of runs row index numbers
+    :param w: size of the runs window
+    :return: flat list of all row indexes marking the end of a run of length w
+    """
     greater_than_w = [run for run in runs if len(run) >= w]
     run_end_indexes = [run[w - 1::w] for run in greater_than_w]
     return [item for sublist in run_end_indexes for item in sublist]
 
 
 def get_trades(runs, data_table):
+    """
+    Construct a list of lists.  Each index in the outer list corresponds to a list of trades where the index equals w -1
+    and w is the window size.  This function will construct this matrix for w equal to 1, 2, 3, 4, and 5
+    :param runs: list of lists of indexes corresponding to negative returns runs
+    :param data_table: matrix of stock data with the first row being the headers
+    :return: list of lists of trades constructed from the runs passed in. each list in the list contains data for
+    w = i + 1 where i is the index in the outer list
+    """
     current_balance = 100  # dollars
     adj_stock_price_idx = data_table[0].index('Adj Close')
     trades = []
@@ -130,26 +190,56 @@ def get_trades(runs, data_table):
 
 
 def get_num_trades(trades):
+    """
+    Helper function to get the number of trades
+    :param trades: list of trades
+    :return: number of trades
+    """
     return len(trades)
 
 
 def get_num_profitable_trades(trades):
+    """
+    Helper function to count the number of profitable trades
+    :param trades: list of trades
+    :return: number of profitable trades
+    """
     return len([trade for trade in trades if trade >= 0])
 
 
 def get_num_losing_trades(trades):
+    """
+    Helper function to count the number of losing trades
+    :param trades: list of trades
+    :return: number of losing trades
+    """
     return len([trade for trade in trades if trade < 0])
 
 
 def get_profit_per_profitable_trade(trades):
+    """
+    Helper function to calculate the average profit per profitable trade
+    :param trades: list of trades
+    :return: mean of profitable trades
+    """
     return statistics.mean([trade for trade in trades if trade >= 0])
 
 
 def get_loss_per_losing_trade(trades):
+    """
+    Helper function to calculate the average loss per losing trade
+    :param trades: list of trades
+    :return: mean of losing trades
+    """
     return statistics.mean([trade for trade in trades if trade < 0])
 
 
 def print_trade_analysis(trades):
+    """
+    Pretty print the trades
+    :param trades: list of trades
+    :return: void
+    """
     print()
     print('Trade Strategy One Analysis')
     print('W\tTrades\t# Profitable Trades\tProfit per Profitable Trade\t# Losing Trades\tLoss per Losing Trade')
@@ -165,6 +255,11 @@ def print_trade_analysis(trades):
 
 
 def get_trades_strategy_two(data_table):
+    """
+    Execute the second trading strategy
+    :param data_table: matrix of stock data with the first row being the headers
+    :return: list of trades
+    """
     total_buys = 0
     total_num_stocks = 0
     current_balance = 100  # dollars
@@ -188,6 +283,11 @@ def get_trades_strategy_two(data_table):
 
 
 def print_strategy_two_trades(trades):
+    """
+    Pretty print the trades returned by get_trades_strategy_two
+    :param trades: list of trades
+    :return: void
+    """
     print()
     print('Trade Strategy Two Analysis')
     print('Trades\t# Profitable Trades\tProfit per Profitable Trade\t# Losing Trades\tLoss per Losing Trade')
@@ -201,6 +301,10 @@ def print_strategy_two_trades(trades):
 
 
 def main():
+    """
+    Main procedure
+    :return: void
+    """
     data_table = get_data_table()
 
     # data broken out by weekday
